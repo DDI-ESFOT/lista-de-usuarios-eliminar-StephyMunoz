ef83accbc02cea851c55e7bf4eb2ccd0d4f0b591
StephyMunoz <58042439+StephyMunoz@users.noreply.github.com>
2021-02-07 16:13:09 -0500

    Eliminación último usuario lista

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,49 +1,54 @@
 /**
  * Created by chalosalvador on 4/2/21
  */
-import { useState } from "react";
+import React, { useState } from "react";
 
 function formatName(user) {
-  return `${user.name} ${user.lastname}`;
+    return user.name + " " + user.lastname;
 }
 
 const UserList = ({ users }) => {
-  const [usersList, setUsersList] = useState(users);
+    const [usersList, setUsersList] = useState(users);
 
-  const handleAddUser = (event) => {
-    // console.log("event", event);
-    console.log("Añadir");
-    const newName = document.querySelector("#name").value;
-    const newLastname = document.querySelector("#lastname").value;
-    const newUser = {
-      name: newName,
-      lastname: newLastname,
+    const handleAddUser = (event) => {
+        const newName = document.querySelector("#name").value;
+        const newLastname = document.querySelector("#lastname").value;
+        const newUser = {
+            name: newName,
+            lastname: newLastname,
+        };
+
+        setUsersList((prevUserList) => {
+            return [...prevUserList, newUser];
+            // return [...prevUserList, newLastname];
+        });
+        document.querySelector("#name").value = "";
+        document.querySelector("#lastname").value = "";
+        console.log(usersList[usersList.length - 1]);
     };
-    console.log("newUser", newUser);
+    const handleDeleteUser = () => {
+        const userToDelete = usersList[usersList.length - 1];
+        console.log("index");
+        setUsersList((prevUserList) => {
+            return prevUserList.filter((usersList) => usersList !== userToDelete);
+        });
+    };
 
-    setUsersList((prevUsersList) => {
-      // Spread operator
-      return [...prevUsersList, newUser];
-    });
-
-    document.querySelector("#name").value = "";
-    document.querySelector("#lastname").value = "";
-  };
-
-  return (
-    <>
-      <h1>Lista de usuarios ({usersList.length} usuarios)</h1>
-      <input type="text" id="name" placeholder="Ingrese un nombre" />
-      <input type="text" id="lastname" placeholder="Ingrese un apellido" />
-      <button onClick={handleAddUser}>Añadir</button>
-      <button>Eliminar el último usuario</button>
-      <ul>
-        {usersList.map((user, index) => {
-          return <li key={Math.random()}>{formatName(user)}</li>;
-        })}
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <h1>Lista de usuarios {usersList.length} usuarios</h1>
+            <input type="text" id="name" placeholder="Ingrese un nombre" />
+            <input type="text" id="lastname" placeholder="Ingrese un apellido" />
+            <button onClick={handleAddUser}>Añadir</button>
+            <button onClick={handleDeleteUser}>Eliminar último usuario</button>
+            <ul>
+                {usersList.map((user, index) => {
+                    return <li key={index}>{formatName(user)}</li>;
+                })}
+            </ul>
+        </>
+    );
 };
 
 export default UserList;
+
